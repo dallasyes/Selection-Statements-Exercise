018d545fe35e484c1c2453c50d7506347b182c98
Dallas Yu <dallas_yu@hotmail.com>
2025-06-24 14:26:47 -0500

    added favorite number guesser

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int favnumber = 42;
+            Console.WriteLine("Guess my favorite number: ");
+            int userInput = int.Parse(Console.ReadLine());
+            if (userInput > favnumber)
+            {
+                Console.WriteLine("Too high!");
+            }
+            else if (userInput < favnumber)
+            {
+                Console.WriteLine("Too low!");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!!!");
+            }
         }
     }
 }
